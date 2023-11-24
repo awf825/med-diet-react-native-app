4a0d5b1eabcb05b0a930fb6c7b7ffdeec392e347
Aiden Flynn <faiden454@gmail.com>
2023-11-24 16:59:31 -0500

    - Consolidating SurveyScreen; form established   - removed AppPicker component   - able to submit form using simple formik setuo   - navigating back to the dashboard on submission

--- src/screens/SurveyScreen.js
+++ src/screens/SurveyScreen.js
@@ -1,26 +1,49 @@
 import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react';
-import AppPicker from "../components/AppPicker/AppPicker"
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   FlatList,
   StyleSheet,
   StatusBar,
   Text,
+  Button,
   View
 } from 'react-native';
 import { BASE_URL } from '../config';
+import { Formik } from 'formik';
+import { Picker } from '@react-native-picker/picker';
+
+let numberOptions = [];
+for (let i = 0; i <= 30; i++) {
+    numberOptions.push({
+        label: String(i),
+        value: i
+    })
+}
+
+let radioOptions = [
+    { label: "Yes", value: 1 },
+    { label: "No", value: 0 }
+]
+
+const getDynamicFormValues = (data) => {    
+    return data.reduce(
+        (prev, curr) => { 
+            return Object.assign(
+                prev, 
+                { 
+                    [curr.field_code]: curr.field_type==="INT" ? 0 : 
+                    curr.field_type==="TEXT" ? 1 :
+                    null
+                }
+            ) 
+        }, 
+        {}
+    );
+}
 
 const SurveyScreen = ({ navigation }) => {
     const [questions, setQuestions] = useState([]);
-
-    let options = [];
-    for (let i = 0; i <= 30; i++) {
-        options.push({
-            label: String(i),
-            value: i
-        })
-    }
 
     useEffect(() => {
         axios.get(`${BASE_URL}/api/questions/`)
@@ -30,27 +53,57 @@ const SurveyScreen = ({ navigation }) => {
         .catch(err => console.log(err))
     }, [])
 
-    const _renderItem = ({item, index}) => (
-        // <FormBlock item={item} index={index} />
-        <View style={styles.item}>
-            {
-                <AppPicker 
-                    label={item.question_text}
-                    options={options} 
-                />
+    const _handleSubmit = (values) => {
+        axios.post(
+            `${BASE_URL}/api/submissions/submit`,
+            questions.map(q => {
+                return {
+                    ...q,
+                    answer_score: values[q.field_code]
+                }
+            })
+        )
+        .then(resp => {
+            if (resp.data.success) {
+                navigation.navigate('Dashboard')
             }
-        </View>
-    );
+        })
+        .catch(err => console.log(err))
+    }
 
     return (
-        <SafeAreaView style={styles.container}>
-            <FlatList
-                testId={"LIST"}
-                data={questions}
-                renderItem={_renderItem}
-                keyExtractor={item => item.question_id}
-            />
-        </SafeAreaView>
+        questions.length > 0 ?
+        <Formik
+            initialValues={getDynamicFormValues(questions)}
+            onSubmit={values => _handleSubmit(values)}
+        >
+            {({ setFieldValue, handleSubmit, values }) => (
+                <SafeAreaView style={styles.container}>
+                    <FlatList
+                        data={questions}
+                        renderItem={({item, index}) => (
+                            <View style={styles.item}>
+                                <Text>{item.question_text}</Text>
+                                <Picker
+                                    selectedValue={values[item.field_code]}
+                                    onValueChange={(itemValue, itemIndex) => {
+                                        setFieldValue(item.field_code, itemValue)
+                                    }}>
+                                    {
+                                        (item.field_type === "INT" ? numberOptions : radioOptions).map(option => {
+                                            return <Picker.Item key={option.value} label={option.label} value={option.value} />
+                                        })
+                                    }
+                                </Picker>
+                            </View>
+                        )}
+                        keyExtractor={item => item.question_id}
+                    />
+                    <Button onPress={handleSubmit} title="DONE"/>
+                </SafeAreaView>
+            )}
+        </Formik> :
+        <Text>LOADING</Text>
     );
 };
 
@@ -60,7 +113,6 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    // backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
