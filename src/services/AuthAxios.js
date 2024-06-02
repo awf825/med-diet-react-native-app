301d93af11225c87bf856d47f33e17da358c47a0
Aiden Flynn <faiden454@gmail.com>
2024-06-02 15:42:48 -0400

    adding support to android for front end axios

--- src/services/AuthAxios.js
+++ src/services/AuthAxios.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
+import { Platform } from 'react-native'
 
 const AuthAxios = (token = null) => {
   // axios instance for making requests
+    // for android emulator, go to system settings and find the ip address of your wifi network
+    // instead of local host
   const axiosInstance = axios.create({
-    // baseURL: 'http://localhost:6868'
-    baseURL: 'http://localhost:8080'
+    baseURL: Platform.OS === "android" ? 'http://100.88.58.246:8080' : 'http://localhost:8080'
+    // baseURL: 'http://100.88.58.246:8080'
   });
 
   // request interceptor for adding token
   axiosInstance.interceptors.request.use((config) => {
+    config.headers["Content-Type"] = "application/json";
     if (token && config.url !== "/api/auth/login/") {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
