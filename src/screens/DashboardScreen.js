a118525f7534c96737ca3c2e7e5fc941f6a43c02
Aiden Flynn <faiden454@gmail.com>
2024-06-02 19:16:36 -0400

    adding in progress bars, scaffolding

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -10,9 +10,10 @@ import {
   ScrollView,
   ActivityIndicator
 } from 'react-native';
+import ProgressBar from 'react-native-animated-progress';
 // https://www.npmjs.com/package/react-native-circular-progress
 // import { AnimatedCircularProgress, CircularProgress } from 'react-native-circular-progress';
-import CircularProgress from 'react-native-circular-progress-indicator';
+import AnimatedCircularProgress from 'react-native-circular-progress-indicator';
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 import AuthAxios from '../services/AuthAxios';
 import { ScoreWidget } from '../components/Widgets/ScoreWidget';
@@ -23,6 +24,7 @@ const DashboardScreen = ({ navigation }) => {
   const [submissions, setSubmissions] = useState([]);
   const [answersByCategory, setAnswersByCategory] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [notEnoughData, setNotEnoughData] = useState(false);
 
   useEffect(() => {
     navigation.addListener('focus', () => {
@@ -38,6 +40,124 @@ const DashboardScreen = ({ navigation }) => {
     });
   }, [navigation]);
 
+  return (
+    <ScrollView style={styles.container}>
+        <>
+          <View style={styles.widgetContainer}>
+            <Text>RECIPE</Text>
+            <View style={styles.textBubble}>
+
+            </View>
+            <Text>----Link to Custom Generator</Text>
+          </View>
+          <View style={styles.widgetContainer}>
+            {
+              notEnoughData ?
+              <Text>Not enoough data</Text> :
+              <>
+                <Text>SURVEY AND PROGRESS</Text>
+                <View style={styles.textBubble}>
+                  <View>
+                    <Text>Submissions: </Text>
+                    <Text>Last submission: </Text>
+                    <Text>Streak: </Text>
+                    <Text>Survey Now</Text>
+                  </View>
+                  <View styles={styles.progressContainer}>
+                    <FlatList
+                      data={answersByCategory}
+                      renderItem={({item}) => <View style={styles.progressItem}>
+                          <View>
+                            <Text>{item.category_display_name}</Text>
+                          </View>
+                          <View>
+                            <ProgressBar
+                                progress={30}
+                                height={5}
+                                backgroundColor="#4a0072"
+                                animated={true}
+                              />
+                          </View>
+                        </View>
+                      }
+                    />
+                  </View>
+                  {/* <View styles={styles.progressContainer}>
+                      {
+                        answersByCategory && answersByCategory.map(item => {
+                          return (
+                            <View style={styles.progressDetailContainer}>
+                              <View style={styles.progressDetail}>
+                                <View>
+                                  <Text>{item.category_display_name}</Text>
+                                </View>
+                                <View>
+                                  <Text>{item.score} / {item.answer_number}</Text>
+                                </View>
+                              </View>
+                              {/* <AnimatedCircularProgress
+                                fill={(item.score / (item.answer_number)) * 100}
+                                tintColor="#00e0ff"
+                                onAnimationComplete={() => console.log('onAnimationComplete')}
+                                backgroundColor="#3d5875"
+                              /> 
+                            </View>
+                          )
+                        })
+                      }
+                  </View> */}
+                </View>
+                <Text>----Link to D-25</Text>
+                <Text>----Link to Submissions</Text>
+              </>
+            }
+          </View>
+          <View style={styles.widgetContainer}>
+            <Text>Tips</Text>
+            <View style={styles.textBubble}>
+
+            </View>
+          </View>
+        </>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    // backgroundColor: '#000',
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  widgetContainer: {
+    padding: 8,
+    margin: "auto",
+    height: 400,
+  },
+  textBubble: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 25,
+    backgroundColor: "red",
+    padding: 15,
+    margin: 5
+  },
+  progressContainer: {
+    marginTop: 8
+    // overflow: "auto",
+
+  },
+  // progressItem: {
+  //   // padding: 4,
+  //   // margin: 4,
+  //   backgroundColor: '#EEEEEE',
+  // }
+});
+
+export default DashboardScreen;
+
+/* AD MOB */
       {/* {
         index % 4 === 0 ?
           <>
@@ -76,47 +196,13 @@ const DashboardScreen = ({ navigation }) => {
           </>
       } */}
 
-  return (
-    <ScrollView style={styles.container}>
-      {
-        answersByCategory.length > 0 &&
-        <>
-          <ScoreWidget answersByCategory={answersByCategory} isLoading={isLoading} />
-          <DualIndicatorWidget answersByCategory={answersByCategory} isLoading={isLoading} />
-        </>
-      }
-    </ScrollView>
-  );
-};
-
-const styles = StyleSheet.create({
-  colorLabel: {
-    width: "auto",
-    height: "auto",
-    backgroundColor: "red"
-  },
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#000',
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  scoresContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'flex-start'
-  },
-  legendContainer: {
-    flexDirection: 'column',
-    flex: 1,
-  },
-  progressIndicator: {
-    width: '50%'
-  },
-  label: {
-    color: '#ecf0f1'
-  },
-});
-
-export default DashboardScreen;
+/* Reusable */
+{/* <ScrollView style={styles.container}>
+{
+  answersByCategory.length > 0 &&
+  <>
+    <ScoreWidget answersByCategory={answersByCategory} isLoading={isLoading} />
+    <DualIndicatorWidget answersByCategory={answersByCategory} isLoading={isLoading} />
+  </>
+}
+</ScrollView> */}
